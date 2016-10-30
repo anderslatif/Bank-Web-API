5868ac10dbad535e524c38a2b96fa813d3eaec20
anders11 <anderslatif@gmail.com>
2016-10-30 05:20:27 +0100

    First edition done. Functional and implementing all API calls.

---
+++ src/js/Scripts/APIcall.js
@@ -0,0 +1,116 @@
+var loadProfileInfo = (function ($) {
+
+    $.getJSON(APIURL+'?what=account_info&apikey='+APIKEY, function (data) {
+
+        $.each(data, function() {
+            $.each(this, function(key, val) {
+
+
+                if (val.currency != undefined && val.amount != undefined) {
+
+                    $('.account_name').parent().append('<div>Name: '+ val.currency +'</div>');
+                    $('.account_amount').parent().append('<div>Amount: '+ val.amount +'</div>');
+                }
+
+            });
+        });
+
+
+    });
+
+}(jQuery));
+
+
+
+
+
+
+
+var getAllOffers = (function ($) {
+
+    $.getJSON(APIURL+'?what=offers&apikey='+APIKEY, function (data) {
+
+        let offers = [], $tbody;
+
+
+        $.each(data, function() {
+            $.each(this, function(key, val) {
+
+                if (val.id != undefined) { //todo change or die
+
+                    offers.push('<tr id="' + key + '"><th scope="row" class="tr_id">'+val.id+'</th><td class="tr_amount">'+val.amount+'</td>' +
+                        '<td class="tr_currency">'+val.currency+'</td><td class="tr_since">'+val.since+'</td>' +
+                        '<td><input type="submit" class="tbl_Update" id="'+ val.id +'" value="Buy" onclick="buySpecificOrder(this);"></td></tr>');
+                }
+
+            });
+        });
+
+        $tbody = $('<tbody />').appendTo('.table');
+
+        //append list items to list
+        $tbody.append(offers);
+    });
+
+}(jQuery));
+
+
+function buySpecificOrder(e) {
+    const id = e.id;
+
+
+    (function ($) {
+        $.getJSON(APIURL+'?what=buy&offer='+ id +'&apikey='+APIKEY, function (data) {
+
+            console.log(data.resp.code);
+            if (data.resp.code === "200") {
+                toastr.success("You just made a purchase.");
+
+                loadProfileInfo;
+                schedule; // to update all information on the site
+            } else {
+                toastr.warning("An error occurred in the transaction.")
+            }
+        });
+    })(jQuery);
+}
+
+
+
+function getASpecificExchangeRate(currency1, currency2) {
+
+    (function ($) {
+        $.getJSON(APIURL+'?what=exchange_rate&from='+currency1+'&to='+currency2+'&apikey='+APIKEY, function (data) {
+
+            if(data.resp.code === "404") {
+                toastr.warning("Currency not found.")
+            } else if (data.resp.code === "200") {
+                toastr.success("Exchange rate: " + data.data.amount)
+            } else {
+                toastr.warning("Something went wrong")
+            }
+        });
+
+    }(jQuery));
+
+}
+
+
+
+function setAnOfferForSale(amount) {
+
+        (function ($) {
+            $.getJSON(APIURL+'?what=sell&amount='+amount+'&&apikey='+APIKEY, function (data) {
+
+                if (data.resp.code === "200") {
+                    toastr.success("You just sold for " + data.data.amount + ".")
+                } else {
+                    toastr.warning("Something went wrong")
+                }
+            });
+
+
+    }(jQuery));
+
+}
+
--- src/js/client.js
+++ src/js/client.js
@@ -12,108 +12,49 @@ const callAPIEvery = 5 * 60 * second;
 
 $(document).ready(function() {
     $('#conversion_submit').click(lookUpConvert);
+    $('#sell_submit').click(readyToSell);
 });
 
 
 
-(function schedule($) {
+var schedule = (function ($) {
 
     setTimeout(arguments.callee, callAPIEvery);
 
 
-
-        // ################### profile info
-
-     $.getJSON(APIURL+'?what=account_info&apikey='+APIKEY, function (data) {
-
-
-         $.each(data, function() {
-             $.each(this, function(key, val) {
-
-
-                 if (val.currency != undefined && val.amount != undefined) {
-
-                     $('.account_name').parent().append('<div>Name: '+ val.currency +'</div>');
-                     $('.account_amount').parent().append('<div>Amount: '+ val.amount +'</div>');
-                 }
-
-
-             });
-         });
-
-
-     });
-
-
-
-
-
-
-
-
-
-
-        // ################### update offers
-
-     // remove resultset if this has already been run
-     //$('.tbody').remove();
-
-    // Get offers, call AJAX
-    $.getJSON(APIURL+'?what=offers&apikey='+APIKEY, function (data) {
-
-
-        let offers = [], $tbody;
-
-
-
-        $.each(data, function() {
-            $.each(this, function(key, val) {
-
-                if (val.id != undefined) { //todo change or die
-
-                    offers.push('<tr id="' + key + '"><th scope="row" class="tr_id">'+val.id+'</th><td class="tr_amount">'+val.amount+'</td>' +
-                        '<td class="tr_currency">'+val.currency+'</td><td class="tr_since">'+val.since+'</td>' +
-                        '<td><input type="submit" class="tbl_Update" id="'+ val.id +'" value="Buy" onclick="buyCurrency(this);"></td></tr>');
-                }
-
-
-            });
-        });
-
-        $tbody = $('<tbody />').appendTo('.table');
-
-        //append list items to list
-        $tbody.append(offers);
-    });
+    loadProfileInfo;
+    getAllOffers;
 
 
     // ################### conversion rates
 
-
-
-
 }(jQuery));
 
 
-let buyCurrency = function (e) {
-    console.log(e.id);
-};
+
 
 let lookUpConvert = function (e) {
-    let Currency1 = $("#Currency1").val();
-    let Currency = $("#Currency2").val();
+    let currency1 = $("#Currency1").val();
+    let currency2 = $("#Currency2").val();
 
-    if (Currency1 != "" && Currency != "") {
-        console.log(Currency1);
-        console.log(Currency);
-    } else if (Currency1 == "" && Currency == "") {
+    if (currency1 != "" && currency2 != "") {
+        getASpecificExchangeRate(currency1, currency2);
+    } else if (currency1 == "" && currency2 == "") {
         toastr.info("You have not filled out the currency IDs.");
-    } else if (Currency1 == "") {
+    } else if (currency1 == "") {
         toastr.info("You have not filled out Currency ID 1.");
-    } else if (Currency == "") {
+    } else if (currency2 == "") {
         toastr.info("You have not filled out Currency ID 2.");
     }
 
+    return false;
+};
+
+
+let readyToSell = function () {
+
+    setAnOfferForSale($("#sell_amount").val());
 
     return false;
 };
+
